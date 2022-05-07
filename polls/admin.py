92e9f9c50345db71ff9cc2f6f26bd271293a37ee
Luan Torres <luantorres360@gmail.com>
2022-05-07 15:40:45 -0300

    Customizando admin page

--- polls/admin.py
+++ polls/admin.py
@@ -1,6 +1,26 @@
+from dataclasses import fields
 from django.contrib import admin
 
-from .models import Question
+from .models import Choice, Question
 
-# Register your models here.
-admin.site.register(Question)
+# class ChoiceInline(admin.StackedInline):
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
+
+    # fields = ['pub_date', 'question_text']
+    fieldsets = [
+        (None,                  {'fields': ['question_text']}),
+        ('Date information',    {'fields': ['pub_date'], 'classes':['collapse']}),
+    ]
+    
+    # Objetos “Choice são editados na mesma página de administração de Question.
+    inlines = [ChoiceInline]
+
+admin.site.register(Question, QuestionAdmin)
